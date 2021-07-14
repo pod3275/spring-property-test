43cc6de2acc3c3089c87c0edcc9f8fd855544ba2
sangheon_lee <sangheon_lee@tmax.co.kr>
2021-07-14 13:35:17 +0900

    modified

--- example/src/main/java/com/pod3275/example/service/ServiceV0.java
+++ example/src/main/java/com/pod3275/example/service/ServiceV0.java
@@ -2,13 +2,18 @@ package com.pod3275.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-@PropertySource("classpath:example.properties")
+@PropertySources({
+  @PropertySource("classpath:example.properties"),
+  @PropertySource(value="/home/tmax/example.properties", ignoreResourceNotFound=true),
+})
+
 public class ServiceV0 implements ServiceBase {
 
   @Autowired
