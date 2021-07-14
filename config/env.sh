76ba302689356216236247dc8c9fa00080988b71
sangheon_lee <sangheon_lee@tmax.co.kr>
2021-07-14 13:48:37 +0900

    modified

--- config/env.sh
+++ config/env.sh
@@ -3,3 +3,6 @@
 ##################################
 if [ -z $ENV_VALUE ]; then export ENV_VALUE="123"; fi
 sed -i "s/%ENV_VALUE%/${ENV_VALUE}/g" /home/tmax/example.properties
+
+if [ -z $SERVER_PORT ]; then export SERVER_PORT="8081"; fi
+sed -i "s/%SERVER_PORT%/${SERVER_PORT}/g" /home/tmax/application.properties
